68254af9e6fbda8f5840b65fb982b0d5ed254001
vshev4enko <viacheslav@hyphe.com>
2023-06-23 01:01:39 +0300

    prepare for hex.pm

--- mix.exs
+++ mix.exs
@@ -1,34 +1,44 @@
 defmodule MintWebsocketClient.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/vshev4enko/mint_websocket_client"
+
   def project do
     [
       app: :mint_websocket_client,
-      name: "MintWebsocketClient",
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.14",
-      start_permanent: Mix.env() == :prod,
       deps: deps(),
-      docs: [
-        main: "MintWebsocketClient",
-        extras: ["README.md"]
-      ]
+      name: "MintWebsocketClient",
+      source_url: @source_url,
+      docs: [source_ref: "v#{@version}", main: "readme", extras: ["README.md"]],
+      description: description(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
-    [
-      extra_applications: [:logger]
-    ]
+    []
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:cowboy, "~> 2.9", optional: true},
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:mint_web_socket, "~> 1.0"}
+    ]
+  end
+
+  defp description() do
+    "Behaviour module for implementing websocket clients."
+  end
+
+  defp package() do
+    [
+      maintainers: ["Viacheslav Shevchenko"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => @source_url}
     ]
   end
 end
